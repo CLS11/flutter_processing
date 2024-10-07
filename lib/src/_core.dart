c0607841bd3dfa7545f29dcb5d94be9fede94b93
CLS11 <crystall.101600@gmail.com>
2024-10-08 00:09:37 +0530

    Core api for painting with a canvas.

---
+++ lib/src/_core.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Processing extends StatelessWidget {
+  const Processing({required this.sketch, super.key});
+
+  final Sketch sketch;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      size: Size.infinite,
+      painter: _SketchPainter(
+        sketch: sketch,
+      ),
+    );
+  }
+}
+
+abstract class Sketch {
+  void setup(){}
+
+  void draw(){}
+
+  late Canvas canvas;
+  late Size size;
+
+  void background({
+    required Color color,
+  }) {
+    assert(canvas != null);
+    assert(size != null);
+
+    final paint = Paint()..color = color;
+    canvas.drawRect(Offset.zero & size, paint);
+  }
+}
+
+class _SketchPainter extends CustomPainter {
+  _SketchPainter({required this.sketch}) : assert(sketch != null);
+
+  final Sketch sketch;
+  @override
+  void paint(Canvas canvas, Size size) {
+    sketch
+      ..canvas = canvas
+      ..size = size
+      ..setup()
+      ..draw();
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    // TODO: implement shouldRepaint
+    throw UnimplementedError();
+  }
+}
