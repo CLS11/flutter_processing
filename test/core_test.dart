5a18733a5078f7f8ce087849dbe8a0df55bd43a2
CLS11 <crystall.101600@gmail.com>
2024-10-08 11:37:57 +0530

    Added the test for different background colors with different sketch painters.

--- test/core_test.dart
+++ test/core_test.dart
@@ -7,23 +7,92 @@ import 'package:golden_toolkit/golden_toolkit.dart';
 
 void main() {
   group('core', () {
-    testGoldens('User can paint with processing widget', (tester) async {
+    testGoldens('User can paint background in setup().', (tester) async {
       tester.binding.window
         ..physicalSizeTestValue = Size(100, 100)
         ..devicePixelRatioTestValue = 1.0;
 
       await tester.pumpWidget(
         Processing(
-          sketch: MyTestSketch(),
+          sketch: PaintBackgroundInSetupSketch(),
         ),
       );
 
-      await screenMatchesGolden(tester, 'core_api');
+      await screenMatchesGolden(tester, 'core_setup_background');
     });
+  });
+
+  testGoldens('User can paint background in draw().', (tester) async {
+    tester.binding.window
+      ..physicalSizeTestValue = Size(100, 100)
+      ..devicePixelRatioTestValue = 1.0;
+
+    await tester.pumpWidget(
+      Processing(
+        sketch: PaintBackgroundInDrawSketch(),
+      ),
+    );
+
+    await screenMatchesGolden(tester, 'core_draw_background');
+  });
+
+  testGoldens('User can paint orange background.', (tester) async {
+    tester.binding.window
+      ..physicalSizeTestValue = Size(100, 100)
+      ..devicePixelRatioTestValue = 1.0;
+
+    await tester.pumpWidget(
+      Processing(
+        sketch: PaintOrangeBackgroundSketch(),
+      ),
+    );
+
+    await screenMatchesGolden(tester, 'core_draw_background_orange');
+  });
+
+  testGoldens('background() in draw() replaces background in setup()',
+      (tester) async {
+    tester.binding.window
+      ..physicalSizeTestValue = Size(100, 100)
+      ..devicePixelRatioTestValue = 1.0;
+
+    await tester.pumpWidget(
+      Processing(
+        sketch: PaintBackgroundInSetupAndDrawSketch(),
+      ),
+    );
+
+    await screenMatchesGolden(tester, 'core_background_setup_draw');
   });
 }
 
-class MyTestSketch extends Sketch {
+class PaintBackgroundInSetupSketch extends Sketch {
+  @override
+  void setup() {
+    background(color: const Color(0xFF404040));
+  }
+}
+
+class PaintBackgroundInDrawSketch extends Sketch {
+  @override
+  void draw() {
+    background(color: const Color(0xFF404040));
+  }
+}
+
+class PaintOrangeBackgroundSketch extends Sketch {
+  @override
+  void draw() {
+    background(color: const Color(0xFFFFCC00));
+  }
+}
+
+class PaintBackgroundInSetupAndDrawSketch extends Sketch {
+  @override
+  void setup() {
+    background(color: const Color(0xFFFF0000));
+  }
+
   @override
   void draw() {
     background(color: const Color(0xFF404040));
