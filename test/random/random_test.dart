12982e197a9a2a189c710abe05359e1fdc24b196
CLS11 <crystall.101600@gmail.com>
2024-10-12 22:37:33 +0530

    Implemented API of random().

---
+++ test/random/random_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_processing/flutter_processing.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Random', () {
+    testWidgets('random()', (tester) async {
+      await tester.pumpWidget(
+        Processing(
+          sketch: Sketch.simple(
+            draw: (s) {
+              s.randomSeed(0);
+              double randomValue = s.random(50);
+              expect(randomValue, 41.27570359435851);
+
+              randomValue = s.random(-50, 50);
+              expect(randomValue, 38.63148172405516);
+            },
+          ),
+        ),
+      );
+    });
+  });
+}
