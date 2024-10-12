3abe045414163c0a7d524a5469d417dcbee1e64c
CLS11 <crystall.101600@gmail.com>
2024-10-12 15:00:38 +0530

    Added a class for ellipse using rect and a method.

--- lib/src/_core.dart
+++ lib/src/_core.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 
 class Processing extends StatelessWidget {
@@ -94,6 +95,12 @@ class Sketch {
       ..drawCircle(center, diameter / 2, _strokePaint);
   }
 
+  void ellipse(Ellipse ellipse) {
+    canvas
+      ..drawOval(ellipse.rect, _fillPaint)
+      ..drawOval(ellipse.rect, _strokePaint);
+  }
+
   void square(Square square) {
     canvas
       ..drawRect(square.rect, _fillPaint)
@@ -179,6 +186,52 @@ class Square {
           center: center,
           width: extent,
           height: extent,
+        );
+
+  final Rect _rect;
+  Rect get rect => _rect;
+}
+
+class Ellipse {
+  Ellipse.fromLTWH({
+    required Offset topLeft,
+    required double width,
+    required double height,
+  }) : _rect = Rect.fromLTWH(
+          topLeft.dx,
+          topLeft.dy,
+          width,
+          height,
+        );
+
+  Ellipse.fromLTRB({
+    required Offset topLeft,
+    required Offset bottomRight,
+  }) : _rect = Rect.fromLTRB(
+          topLeft.dx,
+          topLeft.dy,
+          bottomRight.dx,
+          bottomRight.dy,
+        );
+
+  Ellipse.fromCenter({
+    required Offset center,
+    required double width,
+    required double height,
+  }) : _rect = Rect.fromCenter(
+          center: center,
+          width: width,
+          height: height,
+        );
+
+  Ellipse.fromCenterWithRadius({
+    required Offset center,
+    required double radius1,
+    required double radius2,
+  }) : _rect = Rect.fromCenter(
+          center: center,
+          width: radius1 * 2,
+          height: radius2 * 2,
         );
 
   final Rect _rect;
