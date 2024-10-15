b483673262f3e47197371c95f0e6139f9703ecad
CLS11 <crystall.101600@gmail.com>
2024-10-15 12:23:34 +0530

    Golden Tests for API implementation of size(), height(), width().

---
+++ test/environment/environment_test.dart
@@ -0,0 +1,89 @@
+// ignore_for_file: deprecated_member_use
+
+import 'package:flutter/material.dart';
+import 'package:flutter_processing/src/_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+import '../test_infra.dart';
+
+void main() {
+  group('Environment', () {
+    testGoldens('width()', (tester) async {
+      configureWindowSpecTest(tester);
+      await tester.pumpWidget(
+        Processing(
+          sketch: Sketch.simple(
+            draw: (s) {
+              s
+                ..noStroke()
+                ..background(color: Colors.black)
+                ..rect(rect: Rect.fromLTWH(0, 40, s.width.toDouble(), 20))
+                ..rect(rect: Rect.fromLTWH(0, 60, s.width / 2, 20));
+            },
+          ),
+        ),
+      );
+
+      await screenMatchesGolden(
+        tester,
+        'environment_width-example-1',
+        customPump: (tester) async {
+          await tester.pumpAndSettle();
+        },
+      );
+    });
+
+    testGoldens('height()', (tester) async {
+      configureWindowSpecTest(tester);
+      await tester.pumpWidget(
+        Processing(
+          sketch: Sketch.simple(
+            draw: (s) {
+              s
+                ..noStroke()
+                ..background(color: Colors.black)
+                ..rect(rect: Rect.fromLTWH(40, 0, 20, s.height.toDouble()))
+                ..rect(rect: Rect.fromLTWH(60, 0, 20, s.height / 2));
+            },
+          ),
+        ),
+      );
+
+      await screenMatchesGolden(
+        tester,
+        'environment_height-example-1',
+        customPump: (tester) async {
+          await tester.pumpAndSettle();
+        },
+      );
+    });
+
+    testGoldens('size()', (tester) async {
+      configureWindowSpecTest(tester);
+      //Expanding the canvas size
+      tester.binding.window.physicalSizeTestValue = Size(200, 200);
+      await tester.pump();
+      await tester.pumpWidget(
+        Processing(
+          sketch: Sketch.simple(
+            setup: (s) {
+              s.setSize(width: 500, height: 500);
+            },
+            draw: (s) {
+              s.circle(center: const Offset(50, 50), diameter: 25);
+            },
+          ),
+        ),
+      );
+
+      await screenMatchesGolden(
+        tester,
+        'environment_size-example-1',
+        customPump: (tester) async {
+          await tester.pumpAndSettle();
+        },
+      );
+    });
+  });
+}
