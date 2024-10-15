fa6e72edb2b3c91d8edeebcc6e67d66644e19cd3
CLS11 <crystall.101600@gmail.com>
2024-10-15 14:02:18 +0530

    Adding stars for a purple rain using port processing.

--- example/lib/home_screen.dart
+++ example/lib/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:example/star.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_processing/flutter_processing.dart';
 
@@ -9,31 +10,49 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  Offset _circleOffset = Offset(400, 250);
+  final _stars = <Star>[];
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    _stars.clear();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow,
       body: Center(
-        child: SizedBox(
-          width: 800,
-          height: 500,
-          child: Processing(
-            sketch: Sketch.simple(
-              setup: (s) {
-                s
-                  ..setSize(width: 400, height: 400)
-                  ..background(color: Colors.white);
-              },
-              draw: (s) {
-                s.circle(
-                  center: Size(s.width.toDouble(), s.height.toDouble())
-                      .center(Offset.zero),
-                  diameter: 50,
-                );
-              },
-            ),
+        child: Processing(
+          sketch: Sketch.simple(
+            setup: (s) {
+              final width = 800;
+              final height = 800;
+
+              s
+                ..setSize(width: width, height: height)
+                ..background(color: Colors.black);
+
+              if (_stars.isEmpty) {
+                for (int i = 0; i < 100; ++i) {
+                  _stars.add(
+                    Star(
+                      x: s.random((-width / 2).toInt(), (width / 2).toInt()),
+                      y: s.random((-height / 2).toInt(), (height / 2).toInt()),
+                      z: s.random(width),
+                    ),
+                  );
+                }
+              }
+            },
+            draw: (s) {
+              s.background(color: Colors.black);
+              for (final star in _stars) {
+                star
+                  ..update()
+                  ..paint(s);
+              }
+            },
           ),
         ),
       ),
