fda27097e10bbc48c064c7caffac3a2852fee95d
CLS11 <crystall.101600@gmail.com>
2024-10-16 16:43:32 +0530

    Golden test for implementation of translate().

---
+++ test/transform/transform_test.dart
@@ -0,0 +1,44 @@
+import 'dart:ui';
+
+import 'package:flutter_processing/flutter_processing.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+void main() {
+  group('Transform', () {
+    testGoldens('translate(): example 1', (tester) async {
+      await tester.pumpWidget(
+        Processing(
+          sketch: Sketch.simple(
+            draw: (s) {
+              s
+                ..translate(x: 30, y: 20)
+                ..rect(rect: Rect.fromLTWH(0, 0, 55, 55));
+            },
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
+      await screenMatchesGolden(tester, 'transform_translate-example-1');
+    });
+
+    testGoldens('translate(): example 2', (tester) async {
+      await tester.pumpWidget(
+        Processing(
+          sketch: Sketch.simple(
+            draw: (s) {
+              s
+                ..rect(rect: Rect.fromLTWH(0, 0, 55, 55))
+                ..translate(x: 30, y: 20)
+                ..rect(rect: Rect.fromLTWH(0, 0, 55, 55))
+                ..translate(x: 14, y: 14)
+                ..rect(rect: Rect.fromLTWH(0, 0, 55, 55));
+            },
+          ),
+        ),
+      );
+      await tester.pumpAndSettle();
+      await screenMatchesGolden(tester, 'transform_translate-example-2');
+    });
+  });
+}
