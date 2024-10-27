635ab17d7be18c78278bda6124ed8b0b87a31304
CLS11 <crystall.101600@gmail.com>
2024-10-27 20:26:12 +0530

    Added a golden test for API strokeWeight() with throwing an exception for strokeWeight(-1).

---
+++ test/shapes/attributes.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+
+import 'package:flutter_processing/flutter_processing.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+import '../test_infra.dart';
+
+void main() {
+  group('Shape', () {
+    group('Attributes', () {
+      testGoldens('strokeWeight(): example 1', (tester) async {
+        configureWindowSpecTest(tester);
+        await tester.pumpWidget(
+          Processing(
+            sketch: Sketch.simple(
+              draw: (s) {
+                s
+                  ..noLoop()
+                  ..strokeWeight(1)
+                  ..line(const Offset(20, 20), const Offset(80, 20))
+                  ..strokeWeight(4)
+                  ..line(const Offset(20, 40), const Offset(80, 40))
+                  ..strokeWeight(10)
+                  ..line(const Offset(20, 70), const Offset(80, 70));
+              },
+            ),
+          ),
+        );
+        await screenMatchesGolden(
+          tester,
+          'shape_attributes_stroke-weight-example-1',
+        );
+      });
+
+      testWidgets('strokeWeight(): invalid value', (tester) async {
+        configureWindowSpecTest(tester);
+        await expectLater(
+          () {
+            final sketch = Sketch.simple();
+            sketch.strokeWeight(-1);
+          },
+          throwsA(isA<Exception>()),
+        );
+      });
+    });
+  });
+}
